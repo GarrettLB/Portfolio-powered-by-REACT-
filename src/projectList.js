7ac0129911f892332625f087bc4c29d1d61ec953
GLBryan <garrettleebryan@gmail.com>
2022-02-21 11:25:22 -0500

    added Business Connect and reordered projects

--- src/projectList.js
+++ src/projectList.js
@@ -4,14 +4,22 @@ const NoteAppSS = require('../src/images/NoteAppSS.png');
 const TechReviewSS = require('../src/images/TechReviewSS.png');
 const WeatherSS = require('../src/images/WeatherSS.png');
 const WishListSS = require('../src/images/WishListSS.png');
+const BusinessConnectSS = require('../src/images/BusinessConnectSS.png');
 
 const projectlist = [
     {
-        name: "Tech Review",
-        pic: TechReviewSS,
-        link: "https://gb-tech-review.herokuapp.com/",
-        github: "https://github.com/GarrettLB/Tech-Review",
-        alt: "Tech Review App Screenshot"
+        name: "Business Connect",
+        pic: BusinessConnectSS,
+        link: "https://business-connect-lgat.herokuapp.com/",
+        github: "https://github.com/GarrettLB/Business-Connect",
+        alt: "Business Connect App Screenshot"
+    },
+    {
+        name: "Wish List",
+        pic: WishListSS,
+        link: "https://wish-list-fscg.herokuapp.com/",
+        github: "https://github.com/nandodavila/Wish-List",
+        alt: "Wish List App Screenshot"
     },
     {
         name: "Drink Finder",
@@ -19,6 +27,13 @@ const projectlist = [
         link: "https://bromarito.github.io/drink-finder/",
         github: "https://github.com/brOmarito/drink-finder",
         alt: "Drink Finder App Screenshot"
+    },
+    {
+        name: "Tech Review",
+        pic: TechReviewSS,
+        link: "https://gb-tech-review.herokuapp.com/",
+        github: "https://github.com/GarrettLB/Tech-Review",
+        alt: "Tech Review App Screenshot"
     },
     {
         name: "Weather App",
@@ -39,14 +54,7 @@ const projectlist = [
         link: "https://glb-note-app.herokuapp.com/",
         github: "https://github.com/GarrettLB/Note-App",
         alt: "Note App Screenshot"
-    },
-    {
-        name: "Wish List",
-        pic: WishListSS,
-        link: "https://wish-list-fscg.herokuapp.com/",
-        github: "https://github.com/nandodavila/Wish-List",
-        alt: "Wish List App Screenshot"
-    },
+    }
 ]
 
 export default projectlist
